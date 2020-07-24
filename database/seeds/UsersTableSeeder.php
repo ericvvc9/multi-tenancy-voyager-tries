ed4c48669fc4bca4daa2943b535a95f22711215c
Eric Vargas <ericvvc9@gmail.com>
2020-07-24 00:46:17 -0500

    Avance

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
             User::create([
                 'name'           => 'Admin',
                 'email'          => 'admin@admin.com',
+                'username'          => 'admin',
                 'password'       => bcrypt('kiruadmin'),
                 'remember_token' => Str::random(60),
                 'role_id'        => $role->id,
